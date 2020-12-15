4a315efc5590febbf1ddf992c0af5def6b701aa2
Emerson Duarte <raasjava@gmail.com>
2020-12-15 19:03:05 -0300

    Conectando back com front

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParse = require('body-parser');
+const cors = require('cors')
 
 const app = express();
 
 app.use(bodyParse.json())
 app.use(bodyParse.urlencoded({extended: false}));
+app.use(cors())
 
 require('./app/controllers/authController')(app);
 require('./app/controllers/productController')(app);
--- frontend/src/pages/Product.tsx
+++ frontend/src/pages/Product.tsx
@@ -1,10 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Container, Table } from 'react-bootstrap';
 import Menu from '../components/Menu'
 
+import api from "../services/api";
+
+interface Products {
+    factoryid: number;
+    name: string;
+    description: string;
+    model: string;
+    brand: string;
+    size: string;
+    color: string
+}
+
 export default function Product(){
+
+    const [products, setProducts] = useState<Products>();
+        
+    useEffect(() => {
+        api.get('api/product').then(data => {
+            setProducts(data.data);
+            console.log(data)
+        })
+    },)
+    
+    if(!products){
+        return <p>Carregando...</p>;
+    }
+
     return (
         <><Menu />
-            <h1>Produtos</h1></>
+            <Container>
+            <h1>Produtos</h1>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td >Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </Table>
+            </Container>
+            </>
         );
     
 }
